f83eb7a7c41b5234bb17a7aea07723fe82462cac
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-18 13:14:28 +0530

    Warning ~  Using `&lt;img&gt; (fix)
    ESLint: Using `&lt;img&gt;` could result in slower LCP and higher bandwidth. Consider using `&lt;Image /&gt;` from `next/image` to automatically optimize images. This may incur additional usage or cost from your provider. See: https://nextjs.org/docs/messages/no-img-element (@next/next/no-img-element)

--- app/(site)/components/connect.tsx
+++ app/(site)/components/connect.tsx
@@ -154,6 +154,7 @@ const StreakStats = ({githubProfileLink}: { githubProfileLink: string }) => {
     // Return an img element with the streak stats image and some styles
     return (
         <img
+            alt={`GitHub streak stats of ${username}`}
             style={{verticalAlign: "center"}}
             src={`https://github-readme-streak-stats.herokuapp.com/?user=${username}&theme=transparent&hide_border=false&border_radius=10&exclude_days=Sun%2CMon%2CTue%2CWed%2CThu%2CFri%2CSat`}
         />
