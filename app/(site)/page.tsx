a924791158a41e88bb8f4d97ead094ba8312caaf
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-18 15:00:47 +0530

    Separate the Json file for Data

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,12 @@
 import {User} from "@/app/components/User";
 import {Heading} from "@/app/components/Heading";
+import data from "../../Your Data/Heading.json";
 
 export default function Home() {
     return (
         <div className="main-bg">
             <div className="flex-item">
-                <Heading/>
+                <Heading data={data} />
                 <User/>
             </div>
         </div>
