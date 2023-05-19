26b44b258f1b3d29d693d9a6fb1aa98f67600c3f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-20 01:55:10 +0530

    Backtotop Features
    - Up
    - Down
    - Zoom Effect

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,13 @@
 import {User} from "@/app/components/User";
 import {Heading} from "@/app/components/Heading";
 import data from "../../Your Data/Heading.json";
+import Backtotop from "@/app/(site)/components/Backtotop";
 
 export default function Home() {
     return (
         <div className="main-bg">
             <div className="flex-item">
+                <Backtotop/>
                 <Heading data={data} />
                 <User/>
             </div>
